dec8fac012e094692dcd8e5e477e84ae440b9eda
Praise <thepirofreak@gmail.com>
2021-10-12 18:56:24 +0530

    Updated the Tabel

--- db_connect.py
+++ db_connect.py
@@ -1,5 +1,21 @@
+from piro import db_open_conn, db_close_conn
+# import psycopg2
+    
+# def db_open_conn():
+#     conn = psycopg2.connect(
+#             host = "localhost",
+#             user = "praise",
+#             password = "praise123",
+#             database = "python_praise",
+#             port = "5432"
+#         )
+#     return conn
+# print(db_open_conn)
 
-from piro import db_open_conn,db_close_conn
+# def db_close_conn():
+#   conn = db_open_conn()
+
+#   return conn.close()
 
 
 def test():
@@ -29,14 +45,41 @@ def insert_table():
   conn.autocommit = True
   cursor = conn.cursor()
   cursor.execute('''INSERT INTO EMPLOYEES(employ_name, place, job_type) VALUES ('Ramya', 'Delhi', 'Student')''')
+  cursor.execute('''INSERT INTO STUDENTS(stud_name, place, field) VALUES ('Praise', 'Delhi', 'Computer Science')''')
+  cursor.execute('''INSERT INTO workers(worker_name, place, job_type) VALUES ('Amit', 'Agra', 'Technician')''')
+
 # insert_table()
+
+def del_table():
+  conn = db_open_conn()
+  conn.autocommit = True
+  cursor = conn.cursor()
+  cursor.execute('''DELETE FROM employees WHERE employ_id=2''')
+  cursor.execute('''DELETE FROM students WHERE stud_id=2''')
+  cursor.execute('''DELETE FROM workers WHERE worker_id=2''')
+
+# del_table()
+
+def upd_table():
+  conn = db_open_conn()
+  conn.autocommit = True
+  cursor = conn.cursor()
+  cursor.execute('''UPDATE students SET place='Mumbai' WHERE stud_id=2''')
+
+upd_table()
+
+def alter_table():
+  conn = db_open_conn()
+  conn.autocommit = True
+  cursor = conn.cursor()
+  cursor.execute('''''')
 
 def select_db():
   conn = db_open_conn()
   cursor = conn.cursor()
-  cursor.execute("SELECT employ_name, place from employees")
+  cursor.execute("SELECT * from students")
   result1 = cursor.fetchone()
-  print(result1)
+  # print(result1)
 
   result2 = cursor.fetchall()
   print(result2)
